d4f2c0a432463cf6e56c930379661e626f79667d
alvesrgabriel <gabriel.luky1@gmail.com>
2025-03-10 21:05:46 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import RotasBotao from './components/RotaBotao.js';
+
+export default function App() {
+  return (
+    <RotasBotao>
+      <StatusBar style="auto" />
+    </RotasBotao>
+  );
+}
+
